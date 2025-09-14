9612d6fd60a083c8e188f6682db6c915a12afd3b
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-09-14 08:01:31 +0300

    add buttonVariants for customizable button styles

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,4 +1,24 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
+import { cva } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "rounded-lg text-white px-3 py-3 duration-200 font-medium",
+  {
+    variants: {
+      variant: {
+        solid: "bg-blue-600 hover:bg-blue-700",
+        destructive: "bg-red-600 hover:bg-red-700",
+        secondary: "bg-gray-600 hover:bg-gray-700",
+        outline: "bg-transparent border border-gray-600 hover:bg-gray-600",
+        ghost: "bg-transparent hover:bg-gray-600",
+        link: "bg-transparent hover:bg-transparent",
+      },
+    },
+    defaultVariants: {
+      variant: "solid",
+    },
+  }
+);
 
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactNode;
