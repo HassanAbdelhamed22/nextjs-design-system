a93d13c31a14e61672b264b452fb1fd577e4e95e
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-09-25 12:11:05 +0300

    use configurations for ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider enableSystem attribute="class" defaultTheme="system">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
